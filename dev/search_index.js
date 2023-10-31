6a4c80c5ee9cfb9da052bd8417beefbc49abd7d2
Documenter.jl <documenter@juliadocs.github.io>
2023-10-31 04:58:10 +0000

    build based on 8884373

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RubberBearings","category":"page"},{"location":"#RubberBearings","page":"Home","title":"RubberBearings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RubberBearings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RubberBearings]","category":"page"}]
+}
